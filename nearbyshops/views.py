48e87e0629ef3d61c1e6d401e38fe83b92c88595
Muhamad Fahrur Rasyid <mfahrurashyid@gmail.com>
2020-06-20 10:12:16 -0400

    Completing Index Page

--- nearbyshops/views.py
+++ nearbyshops/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
+
+latitude = 39.290440
+longitude = -76.612330
+
+user_location = Point(longitude, latitude, srid=4326)
 
 # Create your views here.
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = "shops"
+    queryset = Shop.objects.annotate(distance=Distance("location", user_location)).order_by("distance")[0:6]
+    template_name = "shops/index.html"
+
+
